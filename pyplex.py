3b88b3c7fc4da39b046154ad0725514e467866d8
megawubs <bwubs@me.com>
2012-10-28 21:38:31 +0100

    implemented a stopPyplex function. Needs to be called from http://ipofyourpi:3000/xbmcCmds/xbmcHttp?command=stopPyplex()

--- pyplex.py
+++ pyplex.py
@@ -115,6 +115,16 @@ class xbmcCommands:
     def stop(self, message):
         global omx
         omx.stop()
+
+    def stopPyplex(self, message):
+        self.stopOMX()
+        global service
+        pygame.quit()
+        exit()
+        # service.unpublish()
+        
+
+
     def stopOMX(self, message = None):
         if self.OMXRunning():
             os.kill(self.pid, signal.SIGKILL)
@@ -142,19 +152,23 @@ class xbmcCommands:
 
 class image:
     def __init__(self, picture):
+        pygame.init()
         self.picture = pygame.image.load(picture)
         self.picture = pygame.transform.scale(self.picture,(1280,1024))
 
     def set(self):
+        pygame.mouse.set_visible(False)
         pygame.display.set_mode(self.picture.get_size())
         main_surface = pygame.display.get_surface()
         main_surface.blit(self.picture, (0, 0))
         pygame.display.update()
+
 xbmcCmmd = xbmcCommands()
 
 
 if __name__ == "__main__":
     print "starting, please wait..."
+    global service
     service = ZeroconfService(name="Raspberry Plex", port=3000)
     service.publish()
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
